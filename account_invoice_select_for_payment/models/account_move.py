97a276f74ff0e9c3aed1a499b9515a602a30ba75
syera.bonneaux@akretion.com <syera.bonneaux@akretion.com>
2023-12-04 14:29:32 +0100

    [14.0][IMP] add compute in account_invoice_select_for_payment in case of paid

--- account_invoice_select_for_payment/models/account_move.py
+++ account_invoice_select_for_payment/models/account_move.py
@@ -1,13 +1,15 @@
 # Copyright 2020 Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    selected_for_payment = fields.Boolean("To Pay")
+    selected_for_payment = fields.Boolean(
+        string="To Pay", compute="_compute_selected_for_payment", readonly=False
+    )
 
     def action_toggle_select_for_payment(self):
         selected = self.filtered(lambda rec: rec.selected_for_payment)
@@ -16,3 +18,8 @@ class AccountMove(models.Model):
             selected.write({"selected_for_payment": False})
         if unselected:
             unselected.write({"selected_for_payment": True})
+
+    @api.depends("payment_state")
+    def _compute_selected_for_payment(self):
+        if self.payment_state == "paid":
+            self.selected_for_payment = False
